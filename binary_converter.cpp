5d6780a8041a2911a9a3cfa9751bbf97fd6176f7
oliver-burns <oliver_harry_burns@outlook.com>
2025-12-18 17:15:47 +0000

    Update binary_converter.cpp with bug fixes and hexadecimal conversion

--- binary_converter.cpp
+++ binary_converter.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -6,11 +7,40 @@ int main() {
     cout << "Enter a number: ";
     cin >> n;
 
+    // Binary conversion
     cout << "Binary: ";
-    while (n > 0) {
-        cout << (n % 2);
-        n /= 2;
+    if (n == 0) {
+        cout << "0";
+    } else {
+        string binary = "";
+        int temp = n;
+        while (temp > 0) {
+            binary = char('0' + (temp % 2)) + binary;
+            temp /= 2;
+        }
+        cout << binary;
     }
     cout << endl;
+
+    // Hexadecimal conversion
+    cout << "Hexadecimal: ";
+    if (n == 0) {
+        cout << "0";
+    } else {
+        string hex = "";
+        int temp = n;
+        while (temp > 0) {
+            int remainder = temp % 16;
+            if (remainder < 10) {
+                hex = char('0' + remainder) + hex;
+            } else {
+                hex = char('A' + (remainder - 10)) + hex;
+            }
+            temp /= 16;
+        }
+        cout << hex;
+    }
+    cout << endl;
+
     return 0;
 }
